9caf13f6d79e45f6405c090f39b51d3e36d3a30e
iamarjundesai <43983050+iamarjundesai@users.noreply.github.com>
2018-10-09 14:04:57 +0530

    Update arjun2.py

--- arjun2.py
+++ arjun2.py
@@ -1,12 +1,12 @@
-def armn(x):
-     sum=0
-     a=x
-     while (a>0):  #starts while loop
-        l=a%10    #extracts last digit 
-        sum+=l**3 #cube of extracted digit 
-        a=a//10   #gives quotient of the input
-     if sum==x:
-        return "Armstrong No"
+def armn(x):                         #defines function
+     sum=0                           #considers sums value is 0
+     a=x                             #defines variable value to x
+     while (a>0):                    #starts while loop
+        l=a%10                       #extracts last digit 
+        sum+=l**3                    #cube of extracted digit 
+        a=a//10                      #gives quotient of the input
+     if sum==x:                      #checks if sum equals the input
+        return "Armstrong No"        #
      else:
         return "NOT armstrong no"
 x=int(input())
