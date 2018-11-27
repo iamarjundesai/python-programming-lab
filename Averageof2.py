500fcd2323061dbc8d7da286213f46cd58c47b7f
iamarjundesai <43983050+iamarjundesai@users.noreply.github.com>
2018-11-27 12:52:15 +0530

    Update Averageof2.py

--- Averageof2.py
+++ Averageof2.py
@@ -1,4 +1,4 @@
-t1=eval(input("Enter number 1 :"))
-t2=eval(input("Enter number 2 :"))
-print("Average of two numbers is: ")
-print((t1+t2)/2)
+t1=eval(input("Enter number 1 :"))    #allows to enter the first number
+t2=eval(input("Enter number 2 :"))         #allows to enter second number
+print("Average of two numbers is: ")      #prints the initial full line
+print((t1+t2)/2)                          #prints the final output                        
