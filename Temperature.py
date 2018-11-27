31dfae75a0c1e92ed593eb6f8358c78b7fe217e5
iamarjundesai <43983050+iamarjundesai@users.noreply.github.com>
2018-11-27 13:07:06 +0530

    Temperature conversion from celcius to farenheit

--- Temperature.py
+++ Temperature.py
@@ -1,4 +1,4 @@
-print("Enter temperature in celsius")
-c=int(input())
-f=1.8*c+57.6
-print("The temperature in Farhenheit is",f)
+print("Enter temperature in celsius")   
+c=int(input())                          #takes the temperature in celcius
+f=1.8*c+57.6                              #converts the temp
+print("The temperature in Farhenheit is",f)     #prints final output
