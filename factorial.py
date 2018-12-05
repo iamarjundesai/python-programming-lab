ecd8ba527b12937c7858d40852d6f07581a1745e
iamarjundesai <43983050+iamarjundesai@users.noreply.github.com>
2018-12-05 12:36:15 +0530

    finding the factorial

--- factorial.py
+++ factorial.py
@@ -9,4 +9,4 @@ elif num==0:
 else:
      for i in range(1,num+1):
               factorial = factorial*i
-     print("the fact of",num,"is", factorial)
+     print("the fact of",num,"is", factorial) 
