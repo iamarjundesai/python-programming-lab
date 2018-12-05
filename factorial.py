05b1448ee8e8227cdb421a13016623d60f567ae1
iamarjundesai <43983050+iamarjundesai@users.noreply.github.com>
2018-12-05 12:35:15 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+num=int(input("enter the number"))
+
+factorial =1
+
+if num<0:
+   print("Sorry")
+elif num==0:
+   print("fact is one")
+else:
+     for i in range(1,num+1):
+              factorial = factorial*i
+     print("the fact of",num,"is", factorial)
